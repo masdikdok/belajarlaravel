c1a2020c74341fd2308d705bdeb2e0c8a892bca8
masdikdok <dicki123andrea@gmail.com>
2019-12-10 21:13:06 +0700

    update add cache redis

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Http\Request;
 use App\Article;
 use Route;
 
 class ArticleController extends Controller
 {
-    public function index(){
-        $articles = Article::with('tags')->paginate(10);
+    public function index(Request $request){
+        $page = $request->has('page') ? $request->page : 1;
+        $articles = Cache::remember('users_page_' . $page, 10000, function () {
+            return Article::with('tags')->paginate(10);
+        });
+
+        // dd($articles);
+
+        // $articles = Article::with('tags')->paginate(10);
 
         return view('article/index', ['articles' => $articles]);
     }
